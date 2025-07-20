9852c7fde790356d609f8ea375919efa67134b0e
onetuks <onetuks@naver.com>
2025-07-20 18:34:33 +0900

    Fix: github action 실패 대응

--- src/test/java/com/onetuks/csphinxserver/TestcontainersConfiguration.java
+++ src/test/java/com/onetuks/csphinxserver/TestcontainersConfiguration.java
@@ -1,9 +1,8 @@
 package com.onetuks.csphinxserver;
 
 import org.springframework.boot.test.context.TestConfiguration;
-import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
-import org.springframework.context.annotation.Bean;
 import org.testcontainers.containers.MySQLContainer;
+import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.utility.DockerImageName;
 
 @TestConfiguration(proxyBeanMethods = false)
@@ -15,12 +14,19 @@ class TestcontainersConfiguration {
   //    return new MongoDBContainer(DockerImageName.parse("mongo:latest"));
   //  }
 
-  @Bean
-  @ServiceConnection
-  MySQLContainer<?> mysqlContainer() {
-    return new MySQLContainer<>(DockerImageName.parse("mysql:latest"))
-        .withDatabaseName("csphinx")
-        .withUsername("myroot")
-        .withPassword("secret");
-  }
+  @Container
+  MySQLContainer<?> mySQLContainer =
+      new MySQLContainer(DockerImageName.parse("mysql:latest"))
+          .withDatabaseName("csphinx")
+          .withUsername("test")
+          .withPassword("test");
+
+  //  @Bean
+  //  @ServiceConnection
+  //  MySQLContainer<?> mysqlContainer() {
+  //    return new MySQLContainer<>(DockerImageName.parse("mysql:latest"))
+  //        .withDatabaseName("csphinx")
+  //        .withUsername("myroot")
+  //        .withPassword("secret");
+  //  }
 }
