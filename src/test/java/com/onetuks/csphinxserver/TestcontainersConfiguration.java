eac843783d98be044b197da5c80db1b231cd452d
onetuks <onetuks@naver.com>
2025-07-20 18:15:33 +0900

    Fix: github action 실패 대응

--- src/test/java/com/onetuks/csphinxserver/TestcontainersConfiguration.java
+++ src/test/java/com/onetuks/csphinxserver/TestcontainersConfiguration.java
@@ -18,6 +18,9 @@ class TestcontainersConfiguration {
   @Bean
   @ServiceConnection
   MySQLContainer<?> mysqlContainer() {
-    return new MySQLContainer<>(DockerImageName.parse("mysql:latest"));
+    return new MySQLContainer<>(DockerImageName.parse("mysql:latest"))
+        .withDatabaseName("csphinx")
+        .withUsername("myroot")
+        .withPassword("secret");
   }
 }
