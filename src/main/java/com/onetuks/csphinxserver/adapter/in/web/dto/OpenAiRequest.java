cfd3d6635ba8025dcfcd12e6a8a048bddb5c5cd6
onetuks <onetuks@naver.com>
2025-08-18 21:58:38 +0900

    Remove: OpenAPI 통신 코드 제거

--- src/main/java/com/onetuks/csphinxserver/adapter/in/web/dto/OpenAiRequest.java
+++
@@ -1,8 +0,0 @@
-package com.onetuks.csphinxserver.adapter.in.web.dto;
-
-import java.util.List;
-
-public record OpenAiRequest(String model, List<Message> messages) {
-
-  public record Message(String role, String content) {}
-}
--- src/main/java/com/onetuks/csphinxserver/global/exception/NoSuchEntityException.java
+++ src/main/java/com/onetuks/csphinxserver/global/exception/NoSuchEntityException.java
@@ -5,8 +5,4 @@ public class NoSuchEntityException extends RuntimeException {
   public NoSuchEntityException() {
     super();
   }
-
-  public NoSuchEntityException(String message) {
-    super(message);
-  }
 }
